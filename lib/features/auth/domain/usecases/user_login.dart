81dbe48bb5e3d92500fcb2b6abb81ace8b683d46
Hamza <hamzazahid6277@gmail.com>
2024-11-11 13:32:33 +0500

    created login usecase

---
+++ lib/features/auth/domain/usecases/user_login.dart
@@ -0,0 +1,23 @@
+import 'package:fpdart/src/either.dart';
+import 'package:raptor/core/errors/failures.dart';
+import 'package:raptor/core/usecase/usecase.dart';
+import 'package:raptor/features/auth/domain/entity/user.dart';
+import 'package:raptor/features/auth/domain/repository/auth_repository.dart';
+
+class UserLogin implements UseCase<User, UserLoginParams>{
+  final AuthRepository authRepository;
+
+
+  UserLogin(this.authRepository);
+
+  @override
+  Future<Either<Failure, User>> call(UserLoginParams params) async{
+    return await authRepository.loginWithEmailPassword(email: params.email, password: params.password);
+  }
+}
+class UserLoginParams{
+  final String email;
+  final String password;
+
+  UserLoginParams(this.email, this.password);
+}
