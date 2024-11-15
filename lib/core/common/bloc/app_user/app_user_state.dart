15ac33dcce949121bbd50bc3b1f6402d1c4e8883
Hamza <hamzazahid6277@gmail.com>
2024-11-15 16:47:27 +0500

    changed from bloc to cubit temporarily

--- lib/core/common/bloc/app_user/app_user_state.dart
+++ lib/core/common/bloc/app_user/app_user_state.dart
@@ -1,7 +1,11 @@
-part of 'app_user_bloc.dart';
+part of 'app_user_cubit.dart';
 
 @immutable
 sealed class AppUserState {}
 
 final class AppUserInitial extends AppUserState {}
-final class AppUserLoggedIn extends AppUserState {}
+final class AppUserLoggedIn extends AppUserState {
+  User user;
+
+  AppUserLoggedIn(this.user);
+}
